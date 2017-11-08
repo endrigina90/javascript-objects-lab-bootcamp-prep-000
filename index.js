9b989828091f877bb0c68411febbdc3dbde7bbd0
Endri Gina <endrigina90@gmail.com>
2017-11-08 23:14:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ return recipes
 function destructivelyUpdateObjectWithKeyAndValue(recipes,key,value) {
   return Object.assign({}, recipes, {[key]:value})
 }
+function updateObjectWithKeyAndValue(recipes,key,value) {
+  return Object.assign(recipes, {[key]:value})
+}
 function destructivelyDeleteFromObjectByKey(recipes, key) {
 return delete recipes[key]
 }
