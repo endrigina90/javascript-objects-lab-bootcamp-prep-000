d05fc47edb59847072712377647487dc415d8f21
Endri Gina <endrigina90@gmail.com>
2017-11-08 22:26:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ function updateObjectWithKeyAndValue(recipes,key,value) {
 recipes[key] = value
 return recipes
 }
+function updateObjectWithKeyAndValue(recipes,key,value) {
+  return recipes.assign({}, recipes, {[key]:value})
+}
